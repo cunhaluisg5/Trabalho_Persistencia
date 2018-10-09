8f03047fe68a8d59f553fd9210386955e3a87bac
cunhaluisg5 <cunhaluisg5@gmail.com>
2018-10-09 14:45:20 -0300

    Criada classe AlunoDao

---
+++ src/dao/AlunoDao.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Aluno;
+import model.Turma;
+
+/**
+ *
+ * @author Luis
+ */
+public class AlunoDao {
+    private List<Aluno> lista;
+
+    public AlunoDao() {
+        lista = new ArrayList<Aluno>();
+    }
+    
+    public void insereAluno(Aluno aluno){
+        lista.add(aluno);
+    }
+    
+    public Aluno buscarAluno(int matricula){
+        Aluno aluno = null;
+        for(Aluno a : lista){
+            if(a.getMatricula() == matricula){
+                aluno = a;
+            }
+        }
+        return aluno;
+    }
+    
+    public void removerAluno(int matricula){
+        Aluno aluno = buscarAluno(matricula);
+        if(aluno != null){
+            lista.remove(aluno);
+        }
+    }
+    
+    public List<Aluno> listarAlunos(String nome){
+        List<Aluno> alunos = null;
+        List<Turma> turmas = new ArrayList<Turma>();
+        for(Turma t : turmas){
+            if(t.getNome().equals(nome)){
+                for(Aluno a : t.getAlunos()){
+                    alunos = (List<Aluno>) a;
+                }
+            }
+        }
+        return alunos;
+    }
+}
